3e433c1a4f456a7f7e4a7b767c41baad3a8ae667
arshiapiri <arshia.piri8585@gmail.com>
2023-05-07 22:50:35 +0430

    login and signup req handler

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,10 +1,10 @@
 const Users = require("../models/Users");
-const { AppError } = require('../utils/app-error');
+const AppError = require('../utils/app-error');
 
 
 const signup = async (req, res, next) => {
     try {
-        const {
+        const requestBody = {
             fristName = null,
             lastName = null,
             username = null,
@@ -14,7 +14,7 @@ const signup = async (req, res, next) => {
             roleIn = "Blogger"
         } = req.body
 
-        if (!fristName?.trim()||
+        if (!fristName?.trim() ||
             !lastName?.trim() ||
             !username?.trim() ||
             !password?.trim() ||
@@ -23,18 +23,50 @@ const signup = async (req, res, next) => {
                 new AppError(400, "fristName, lastName, username, password, phoneNumber is required."));
         }
 
-        const user = Users.find(user => user.username === username);
-
+        let user = await Users.findOne({ username: requestBody.username });
         if (!!user) {
-			return next(
-				new AppError(
-					400,
-					`username: ${username} is already taken, try another one.`
-				)
-			);
-		}
-        await user.save();
+            return next(
+                new AppError(
+                    400,
+                    `username: ${username} is already taken, try another one.`
+                )
+            );
+        }
+        user = await Users.findOne({ phoneNumber: requestBody.phoneNumber });
+        if (!!user) {
+            return next(
+                new AppError(
+                    400,
+                    `phoneNumber: ${phoneNumber} is already taken, try another one.`
+                )
+            );
+        }
+        const newUser = new Users(requestBody);
+        await newUser.save()
+        res.status(201).send(newUser);
     } catch (error) {
-
+        next(new AppError(400, "Sign-up failed. Please check your information and try again."));
     }
 }
+const login = async (req, res, next) => {
+    try {
+        const requestBody = { username = null, password = null } = req.body;
+
+        if (!username?.trim() || !password?.trim()) {
+            return next(new AppError(400, 'username and password is required.'));
+        }
+        let user = await Users.findOne({ username: requestBody.username, password: requestBody.password });
+
+        if (!user) {
+            return next(new AppError(401, 'username or password not match'));
+        }
+
+        res.send({ user })
+    } catch (error) {
+        console.log(error);
+        next(new AppError(500, '[-]: auth controller > login'));
+    }
+}
+
+module.exports = signup
+module.exports = login
