914ff4335a4b984f3e30040ffc45af3e25039acb
arshiapiri <arshia.piri8585@gmail.com>
2023-05-09 14:28:12 +0430

    validation login handel

--- public/javascripts/LogIn.js
+++ public/javascripts/LogIn.js
@@ -12,11 +12,26 @@ $(() => {
         body: JSON.stringify(fields)
       })
         .then(async response => {
+          if (!response.ok) {
+            const errorMessage = await response.text();
+            const parsedError = JSON.parse(errorMessage);
+            //Joi Check validation 
+            const errorText = parsedError.message; 
+            swal({
+              title: "Error!",
+              text: errorText,
+              icon: "error",
+            });
+          } else {
+            swal({
+              title: "Success!",
+              text: "Your data has been saved.",
+              icon: "success",
+            });
             setTimeout(() => {
               window.location.href = `/profile`;
             }, 2000)
-        }).catch(error => {
-            console.log(error);
+          }
         })
     });
   })
