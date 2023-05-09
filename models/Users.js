06483250978014e2a34624920601f5dad96988c9
arshiapiri <arshia.piri8585@gmail.com>
2023-05-09 14:27:53 +0430

    install bcrypt and hash password

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
-const  { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt")
 
 const UsersSchema = new Schema({
     fristName: {
@@ -18,18 +19,18 @@ const UsersSchema = new Schema({
         lowercase: true
     },
     username: {
-        type:String,
-        required:[true, "username is required"],
+        type: String,
+        required: [true, "username is required"],
         minlength: [3, "username must be equal or more than 3 characters"],
         maxlength: [30, "username must be equal or less than 30 characters"],
-        unique:true
+        unique: true
     },
     password: {
         type: String,
         required: [true, "password is required"],
         minlength: [8, "password must be equal or more than 8 characters"],
         maxlength: [30, "password must be equal or less than 30 characters"],
-      },
+    },
     gender: {
         type: String,
         enum: {
@@ -45,14 +46,14 @@ const UsersSchema = new Schema({
         required: true,
         unique: true,
         validate: {
-          validator: function (v) {
-            return /^(\+98|0)?9\d{9}$/.test(v);
-          },
-          message: (props) =>
-            `${props.value} is not a valid Iranian phone number!`,
+            validator: function (v) {
+                return /^(\+98|0)?9\d{9}$/.test(v);
+            },
+            message: (props) =>
+                `${props.value} is not a valid Iranian phone number!`,
         },
         set: function (v) {
-          return `+98${v.replace(/^0/, "")}`;
+            return `+98${v.replace(/^0/, "")}`;
         }
     },
     roleIn: {
@@ -73,5 +74,16 @@ const UsersSchema = new Schema({
     }
 );
 
+UsersSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
-module.exports =  Users =  model("Blogger", UsersSchema)
+    this.password = await bcrypt.hash(this.password, 12);
+
+    next();
+});
+
+UsersSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+};
+
+module.exports = Users = model("Blogger", UsersSchema)
