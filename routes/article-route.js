160c761cc7cdc9a00d01abe243a443947206163d
arshiapiri <arshia.piri8585@gmail.com>
2023-05-23 11:55:30 +0330

    check for create article

--- routes/article-route.js
+++ routes/article-route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const article = require("../controllers/article-controller")
+const  Upload  = require("../utils/multer");
 
 
 router.get("/getAll" , article.getAll);
-router.post("/" , article.create);
+router.post("/create" ,Upload.articleTumbnailUpload.single("thumbnail"), article.create);
 
 module.exports = router
---
+++ validators/checkForCreateArticle.js
@@ -0,0 +1,39 @@
+const Joi = require("joi")
+
+const validationForCreate = Joi.object({
+    title: Joi.string().min(3).max(30).required().messages({
+        "string.base": "Title must be a string",
+        "string.empty": "Title is required",
+        "string.min": "Title must have at least {#limit} characters",
+        "string.max": "Title can have at most {#limit} characters",
+        "any.required": "Title is required",
+      }),
+      description: Joi.string().min(3).max(30).messages({
+        "string.base": "Sketch must be a string",
+        "string.min": "Sketch must have at least {#limit} characters",
+        "string.max": "Sketch can have at most {#limit} characters",
+      }),
+      // thumbnailFileName: Joi.string().required().messages({
+      //   "string.base": "Thumbnail file name must be a string",
+      //   "string.empty": "Thumbnail file name is required",
+      //   "any.required": "Thumbnail file name is required",
+      // }),
+      content: Joi.string().min(3).required().messages({
+        "string.base": "Content must be a string",
+        "string.empty": "Content is required",
+        "string.min": "Content must have at least {#limit} characters",
+        "any.required": "Content is required",
+      }),
+      // imageFileNames: Joi.array().items(Joi.string()).messages({
+      //   "array.base": "Image file names must be an array",
+      //   "array.items": "Image file names must be strings",
+      // }),
+      // author: Joi.objectId().required().messages({
+      //   "string.base": "Author must be a valid ObjectId",
+      //   "any.required": "Author is required",
+      // }),
+})
+
+module.exports.validateUser = function(requestBody) {
+    return validationForCreate.validate(requestBody);
+  };
