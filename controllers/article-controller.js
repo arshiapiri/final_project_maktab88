39de2467faf12fd0dfe583b3486f558e50043111
arshiapiri <arshia.piri8585@gmail.com>
2023-06-05 10:49:09 +0330

    change create and update

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,11 +1,16 @@
 const Articles = require("../models/Article");
 const AppError = require('../utils/app-error');
 const { join } = require('node:path');
-const mongoose = require("mongoose");
+const ArticleForCreate = require("../validators/checkForCreateArticle")
+
+const resizeArticleThumbnail = require('../utils/resizeImage/resizeArticleThumbnail');
+// const resizeArticleImages = require('../utils/resizeImage/resizeArticleImage');
+
 
 module.exports.getAll = async (req, res, next) => {
   try {
-    const readArticle = await Articles.find({});
+    const readArticle = await Articles.find({})
+
 
     res.send({ readArticle })
   } catch (error) {
@@ -24,32 +29,41 @@ module.exports.getId = async (req, res, next) => {
   }
 }
 
+module.exports.updateArticle = async (req, res, next) => {
+  try {
+    const ress = await Articles.findById(req.params.articleId);
+    console.log(ress);
+   
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports.create = async (req, res, next) => {
   try {
     const newArticle = new Articles({
       title: req.body.title,
-      description: req.body.description,
       content: req.body.content,
-      images: req.body.images,
-      author: req.body.author,
-      thumbnail: "/images/thumbnailPic/" + req.file.filename,
+       author: req.session.user._id,
+      thumbnail: "/images/articles/thumbnailPic/" + req.file.filename,
     });
 
-    const creatArticle = await newArticle.save();
-    req.session.Articles = creatArticle;
-
-
+    if (!req.session.user) return res.redirect("/users/login");
+    // const articleImages = await resizeArticleImages.resizeArticleImages(article._id, req.file);
+  
+     await newArticle.save();
+     res.redirect("/Article");
+  
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
+    next(new AppError(500, "something went wrong, your article not create :)"));
   }
 }
 
 module.exports.delete = async (req, res, next) => {
   try {
-    const deleteArticle = await Articles.findByIdAndRemove(new mongoose.Types.ObjectId(req.params.articleId));
-    const readArticleById = await Articles.findById(req.params.articleId);
-    console.log(readArticleById);
-    res.render(join(__dirname, '../views/bloges.ejs'));
+    await Articles.findByIdAndDelete(req.params.articleId)
+    res.redirect('/article')
   } catch (error) {
     console.log(error);
     next(new AppError(500, 'Something went wrong, not your fault :)'));
